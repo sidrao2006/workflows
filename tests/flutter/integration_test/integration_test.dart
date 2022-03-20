9f4bd078bdee5395b6dcaf5eaaf12424cee517a1
Aneesh Rao <sidrao2006@gmail.com>
2022-03-20 18:58:33 +0530

    Update flutter integration tests

--- tests/flutter/integration_test/integration_test.dart
+++ tests/flutter/integration_test/integration_test.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 import 'package:test_app/main.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
