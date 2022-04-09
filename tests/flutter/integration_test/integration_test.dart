562ee47e4444b626d4e8e8d241b7e58a9499d5e6
Aneesh Rao <sidrao2006@gmail.com>
2022-04-09 19:04:03 +0530

    test(flutter fix): Make flutter test app publishable to test pub_pre_check

--- tests/flutter/integration_test/integration_test.dart
+++ tests/flutter/integration_test/integration_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:test_app/main.dart';
+import 'package:test_app/test_app.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
